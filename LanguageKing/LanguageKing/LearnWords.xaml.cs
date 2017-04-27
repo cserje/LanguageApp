8b2e06ca740a47546c1f18eaf8d9989ca7255d2d
Kristóf Jenő Cserjési <cserjesik@sch.bme.hu>
2017-04-28 00:06:30 +0200

    Random mentés

--- LanguageKing/LanguageKing/LearnWords.xaml.cs
+++ LanguageKing/LanguageKing/LearnWords.xaml.cs
@@ -73,12 +73,16 @@ namespace LanguageKing
         {
             //sorrend: angol, francia, német, magyar, olasz
 
-            words.Add(new Word("one", "un, une", "ein", "egy", "uno"));
+            words.Add(new Word("one", "une", "ein", "egy", "uno"));
             words.Add(new Word("two", "deux", "zwei", "kettő", "duo"));
             words.Add(new Word("three", "trois", "drei", "három", "tre"));
             words.Add(new Word("four", "quatre", "vier", "négy", "quattro"));
             words.Add(new Word("five", "cinq", "fünf", "öt", "cinque"));
-
+            words.Add(new Word("six", "six", "sechs", "hat", "sei"));
+            words.Add(new Word("seven", "sept", "sieben", "hét", "sette"));
+            words.Add(new Word("eight", "huit", "acht", "nyolc", "otto"));
+            words.Add(new Word("nine", "neuf", "neun", "kilenc", "nove"));
+            words.Add(new Word("ten", "dix", "zehn", "tíz", "dieci"));
         }
 
         private void GetWord()
